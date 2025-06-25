f57aecae68b7fe8f770ef01e7b00080e8d19e61c
Staucher03 <philipstaudi@gmail.com>
2025-06-25 23:09:43 +0200

    coooommiiiit

--- public/Diagrams.js
+++ public/Diagrams.js
@@ -974,7 +974,7 @@ document.addEventListener('DOMContentLoaded', () => {
     data: {
       labels: ['Trump', 'Musk', 'JD Vance', 'Weißes Haus'],
       datasets: [{
-        label: 'Anzahl der Posts',
+        label: 'Anzahl kategorisierter Posts',
         data: [702, 193, 27, 230],
         backgroundColor: [
           '#B22234', // Flag‑Rot
